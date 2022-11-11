b0228565a187950806cf9ba5ad6b24fa437f3edf
hofzzy <hofzzy@gmail.com>
2022-11-11 18:21:33 +0900

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,324 @@
 import 'package:flutter/material.dart';
 
+// Flutter の Material Design 3 への対応状況について
+// refs: https://github.com/flutter/flutter/issues/91605
+
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        // colorScheme: const ColorScheme(
+        //   brightness: Brightness.light,
+        //   primary: Colors.teal,
+        //   onPrimary: Colors.white,
+        //   secondary: Colors.orange,
+        //   onSecondary: Colors.white,
+        //   error: Colors.red,
+        //   onError: Colors.white,
+        //   background: Colors.white,
+        //   onBackground: Colors.black87,
+        //   surface: Colors.white,
+        //   onSurface: Colors.black87,
+        // ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 57),
+          displayMedium: TextStyle(fontSize: 45),
+          displaySmall: TextStyle(fontSize: 36),
+          headlineLarge: TextStyle(fontSize: 32),
+          headlineMedium: TextStyle(fontSize: 28),
+          headlineSmall: TextStyle(fontSize: 24),
+          titleLarge: TextStyle(fontSize: 22),
+          titleMedium: TextStyle(fontSize: 16),
+          titleSmall: TextStyle(fontSize: 14),
+          bodyLarge: TextStyle(fontSize: 16),
+          bodyMedium: TextStyle(fontSize: 14),
+          bodySmall: TextStyle(fontSize: 12),
+          labelLarge: TextStyle(fontSize: 14),
+          labelMedium: TextStyle(fontSize: 12),
+          labelSmall: TextStyle(fontSize: 11),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: const Text('Material Design 3'),
+        actions: [
+          Builder(builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {},
+            );
+          }),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Header(title: 'Buttons'),
+                Content(
+                  children: [
+                    const FilledButton(),
+                    const FilledTonalButton(),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Elevated Button'),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: const Text('Outlined Button'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Text Button'),
+                    ),
+                  ],
+                ),
+                const Divider(),
+                const Header(title: 'Icon buttons'),
+                Content(
+                  children: [
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.favorite_outline)),
+                    IconButton(
+                        onPressed: () {}, icon: const Icon(Icons.favorite)),
+                    const SizedBox(width: 36),
+                    IconButton(
+                      onPressed: () {},
+                      style: IconButton.styleFrom(
+                        foregroundColor:
+                            Theme.of(context).colorScheme.onPrimary,
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                      ),
+                      icon: const Icon(Icons.favorite),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      style: IconButton.styleFrom(
+                        foregroundColor:
+                            Theme.of(context).colorScheme.onSecondary,
+                        backgroundColor:
+                            Theme.of(context).colorScheme.secondary,
+                      ),
+                      icon: const Icon(Icons.favorite),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      style: IconButton.styleFrom(side: const BorderSide()),
+                      icon: const Icon(Icons.favorite),
+                    ),
+                  ],
+                ),
+                const Divider(),
+                const Header(title: 'Cards'),
+                const Content(
+                  children: [
+                    Card(child: CardContent(text: 'Elevated Card')),
+                    FilledCard(),
+                    OutlinedCard(),
+                  ],
+                ),
+                const Divider(),
+                const Header(title: 'Chips'),
+                Content(
+                  children: [
+                    InputChip(
+                      label: const Text('Input'),
+                      deleteIcon: const Icon(Icons.close, size: 20),
+                      onDeleted: () {},
+                    ),
+                    FilterChip(
+                      label: const Text('Filter'),
+                      selected: true,
+                      onSelected: (value) => print(value),
+                    ),
+                    const ChoiceChip(
+                      label: Text('Choice'),
+                      selected: true,
+                    ),
+                    ActionChip(
+                      avatar: const Icon(Icons.calendar_today),
+                      label: const Text('Assist'),
+                      onPressed: () {},
+                    ),
+                    ActionChip(
+                      label: const Text('Suggestion'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 72),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => _showAlertDialog(context),
+        tooltip: 'Show Dialog',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Account'),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showAlertDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Lorem ipsum'),
+          content: const Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+          actions: [
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final String title;
+
+  const Header({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8, bottom: 12),
+      child: Text(title, style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
+}
+
+class Content extends StatelessWidget {
+  final List<Widget> children;
+
+  const Content({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(spacing: 8, runSpacing: 8, children: children);
+  }
+}
+
+class FilledButton extends StatelessWidget {
+  const FilledButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 0,
+      ),
+      child: const Text('Filled Button'),
+    );
+  }
+}
+
+class FilledTonalButton extends StatelessWidget {
+  const FilledTonalButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Theme.of(context).colorScheme.onSecondaryContainer,
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        elevation: 0,
+      ),
+      child: const Text('Filled Tonal Button'),
+    );
+  }
+}
+
+class FilledCard extends StatelessWidget {
+  const FilledCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Theme.of(context).colorScheme.surfaceVariant,
+      child: const CardContent(text: 'Filled Card'),
+    );
+  }
+}
+
+class OutlinedCard extends StatelessWidget {
+  const OutlinedCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.outline,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      child: const CardContent(text: 'Outlined Card'),
+    );
+  }
+}
+
+class CardContent extends StatelessWidget {
+  final String text;
+
+  const CardContent({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, top: 64, right: 64, bottom: 16),
+      child: Text(text),
     );
   }
 }
